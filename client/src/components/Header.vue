bf994162ff2a40f38e49a26159acd313537fc831
BlueJoeR <joe8773@gmail.com>
2019-09-13 19:41:41 +0800

    Add header component

---
+++ client/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar
+    flat
+  >
+    <v-img
+      :src="require('../assets/logo.svg')"
+      max-height="40"
+      max-width="40"
+      contain
+    ></v-img>
+    <v-toolbar-title>Love Meal</v-toolbar-title>
+
+    <div class="flex-grow-1"></div>
+
+    <v-toolbar-items>
+      <v-btn text v-blur v-if="!isLogin">Sign in</v-btn>
+      <v-btn text v-blur v-if="!isLogin">Sign up</v-btn>
+      <v-btn text v-blur v-if="isLogin">Sign out</v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
+
+<script>
+  export default { 
+    data:() => ({
+      isLogin: false
+    })
+  }
+</script>
