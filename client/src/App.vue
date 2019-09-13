c39c5e363b1f178b031777085d0cd2a289b5917e
BlueJoeR <joe8773@gmail.com>
2019-09-13 21:11:37 +0800

    Add Header component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,36 +1,29 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+    <Header/>
 
     <v-content>
-      <HelloWorld/>
+      <Home/>
     </v-content>
+
+    <Footer></Footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+  import Home from './views/Home'
+  import Header from './components/Header'
+  // import Footer from './components/Footer'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
-};
+  export default {
+   name: 'App',
+   components: {
+     Header,
+    //  Footer,
+     Home,
+   },
+   data:() => ({
+
+   }),
+  };
 </script>
