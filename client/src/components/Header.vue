6ecd73ce408fbebe758563ae58fd845ae2d5d21b
BlueJoeR <joe8773@gmail.com>
2019-09-24 06:11:05 +0800

    Add User menu with dropdown table

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -4,38 +4,88 @@
     flat
     dark
     max-height="60"
-    color="orange darken-2"
+    color="green"
   >
-    <v-img
-      :src="require('../assets/logo.svg')"
-      max-height="40"
-      max-width="40"
-      contain
-    ></v-img>
-    <v-toolbar-title>Love Meal</v-toolbar-title>
-
-    <!-- <v-btn
-      @click="isLogin = !isLogin; drawer = 'null'"
-    >
-      login: {{ isLogin }}
-    </v-btn> -->
+    <!-- Logo -->
+    <router-link to="/">
+      <v-img
+        :src="require('../assets/driver.svg')"
+        max-height="40"
+        max-width="40"
+        contain
+      ></v-img>
+    </router-link>
+    <v-toolbar-title>食載有你</v-toolbar-title>
 
     <div class="flex-grow-1"></div>
 
-    <v-toolbar-items v-if="!isLogin">
-      <v-btn text v-blur>Sign in</v-btn>
-      <v-btn text v-blur>Sign up</v-btn>
+    <!-- Before sign in -->
+    <v-toolbar-items 
+      v-if="!isLogin"
+    >
+      <v-btn text v-blur>SIGN IN</v-btn>
+      <v-btn text v-blur class="d-none d-md-flex" to="signup">SIGN UP</v-btn>
     </v-toolbar-items>
-    <v-menu bottom left>
+    <!-- After sign in -->
+    <v-menu 
+      v-else
+      bottom
+      left
+      offset-y
+      content-class="dropdown-menu"
+      transition="slide-y-transition"
+    >
       <template v-slot:activator="{ on }">
         <v-btn
           dark
           icon
           v-on="on"
         >
-          <v-icon>mdi-dots-vertical</v-icon>
+          <v-avatar
+            color="grey lighten-1"
+            size="40"
+          >
+            <img 
+              v-if="user.img"
+              :src="user.img"
+            >
+            <v-icon v-else>mdi-account</v-icon>
+          </v-avatar>
         </v-btn>
       </template>
+      <!-- User dropdown menu -->
+      <v-card>
+        <v-list
+          flat
+          nav
+        >
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title> {{ user.name }} </v-list-item-title>
+              <v-list-item-subtitle> {{ user.email }} </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider />
+          
+          <v-list-item-group
+            color="green"
+            v-model="featureChoose"
+          >
+            <v-list-item 
+              v-for="(item ,i) in features"
+              :key="i"
+            >
+              <v-list-item-icon>
+                <v-icon> {{ item.icon }} </v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <router-link tag="label" :to="item.link"> {{ item.name }} </router-link>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-card>
     </v-menu>
   </v-app-bar>
 </template>
@@ -43,7 +93,45 @@
 <script>
   export default { 
     data:() => ({
-      isLogin: false,
-    })
+      isLogin: true,
+      featureChoose: null,
+      features: [
+        {
+          name: 'Order',
+          icon: 'mdi-food',
+          link: '/order',
+        },
+        {
+          name: 'History',
+          icon: 'mdi-history',
+          link: '/history',
+        },
+        {
+          name: 'QR code',
+          icon: 'mdi-qrcode-scan',
+          link: '/qrcode',
+        },
+        {
+          name: 'Progress',
+          icon: 'mdi-map-marker',
+          link: '/driver',
+        },
+        {
+          name: 'Settings',
+          icon: 'mdi-settings-outline',
+          link: '/settings',
+        },
+        {
+          name: 'Sign out',
+          icon: 'mdi-logout',
+          link: '/signout',
+        },
+      ],
+      user: {
+        img: 'https://cdn.vuetifyjs.com/images/john.jpg',
+        name: 'Blue Joe',
+        email: 'joe8773@gmail.com',
+      },
+    }),
   }
 </script>
