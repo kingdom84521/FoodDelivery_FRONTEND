4ca222f22091b36004511862188511d53264cf65
黃耑霖 <kingdomhwang@gmail.com>
2019-10-27 15:57:15 +0800

    Fix environment

--- client/src/App.vue
+++ client/src/App.vue
@@ -16,18 +16,15 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
   components: {
-    HelloWorld,
   },
   data: () => ({
     //
