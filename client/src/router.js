d54f4244ba6730ed485c7fe031d35aa6e79b28a4
黃耑霖 <kingdomhwang@gmail.com>
2019-10-28 18:10:59 +0800

    basically structure finished

--- client/src/router.js
+++ client/src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Dashboard from '@/views/dashboard'
 
 Vue.use(Router)
 
@@ -7,5 +8,10 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: Dashboard
+    }
   ]
 })
---
+++ client/src/views/dashboard.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container>
+      <v-row>
+          <v-col
+            cols="12"
+          >
+            <v-card outlined class="text-center">
+                <v-row
+                    justify="center"
+                >
+                    <v-col
+                      cols="6"
+                    >
+                        <v-avatar
+                            color="grey"
+                            size="150"
+                        >
+                            <v-img src="@/assets/logo.png"/>
+                        </v-avatar>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <feature-square v-for="i in 6" :key="i"/>
+                </v-row>
+            </v-card>
+          </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+import FeatureSquare from "@/components/featureSquare"
+
+export default {
+    name: "Dashboard",
+    components: {
+        FeatureSquare
+    },
+    data: () => {
+        return {
+            feature: [
+                {
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
