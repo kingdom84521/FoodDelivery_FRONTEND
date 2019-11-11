6ca7c80bccf3cc0b0e09bce0df7a40d7e2d42f87
黃耑霖 <kingdomhwang@gmail.com>
2019-11-11 18:07:24 +0800

    dashboard icon finished

--- client/src/views/dashboard.vue
+++ client/src/views/dashboard.vue
@@ -1,42 +1,77 @@
 <template>
-  <v-container>
-  </v-container>
+    <v-container fluid>
+        <v-sheet>
+            <v-row>
+                <v-col
+                    class="text-center"
+                    cols="3"
+                >
+                    <v-row>
+                        <v-col>
+                            <v-avatar
+                                size="175"
+                                color="grey"
+                                class="mt-3"
+                            >
+                                <img :src="src" alt="alt">
+                            </v-avatar>
+                        </v-col>
+                    </v-row>
+                    <v-row
+                        dense
+                    >
+                        <v-col>
+                            <div>
+                                {{ account.name }}
+                            </div>
+                        </v-col>
+                    </v-row>
+                    <v-row
+                        no-gutters
+                        justify="center"
+                    >
+                        <v-col
+                            cols="1"
+                        >
+                            <v-icon 
+                                size="15"
+                                left
+                            >
+                                mdi-pencil
+                            </v-icon>
+                        </v-col>
+                        <v-col
+                            cols="5"
+                        >
+                            <div
+                                style="text-decoration: underline;"
+                            >
+                                編輯用戶頭像
+                            </div>
+                        </v-col>
+                    </v-row>
+                </v-col>
+                <v-col
+                    cols="9"
+                >
+
+                </v-col>
+            </v-row>
+        </v-sheet>
+    </v-container>
 </template>
 
 <script>
-// import FeatureSquare from "@/components/dashboard/featureGroup/featureSquare"
-import FeatureGroup from "@/components/dashboard/featureGroup/main"
-
 export default {
     name: "Dashboard",
     components: {
-        FeatureGroup
     },
     data: () => {
         return {
-            features: [   
-            ]
-        }
-    },
-    async mounted() {
-        let el =  {
-                    title: "test",
-                    description: "testing testing testing",
-                    route: "/testing"
-                }
-
-        for ( let i = 0 ; i < 6 ; ++i ) {
-             this.features.push( el )
-             await this.delayOne()
-        }
-    },
-    methods: {
-        delayOne() {
-            return new Promise( resolve => {
-                setTimeout( () => {
-                    resolve()
-                }, 100 )
-            })
+            src: "https://ui-avatars.com/api/?name=John+Doe&size=175",
+            account: {
+                name: "default"
+            }
         }
     }
 }
