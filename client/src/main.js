075afd6d2f40ff9ca1ec9738a893dabce559575c
黃耑霖 <kingdomhwang@gmail.com>
2019-11-12 00:13:48 +0800

    add lodash to vue prototype

--- client/src/main.js
+++ client/src/main.js
@@ -4,9 +4,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import _ form 'lodash'
 
 Vue.config.productionTip = false
+
+Vue.prototype._ = _
 
 new Vue({
   router,
