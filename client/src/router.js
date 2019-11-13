fe8b80f6c6c640067cbdb2ec015ddf89e884e979
bleach12326 <bleach12326@gmail.com>
2019-11-13 14:02:08 +0800

    first init.

--- client/src/router.js
+++ client/src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import OrderManage from "OrderManage.vue"
 
 Vue.use(Router)
 
@@ -7,5 +8,10 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    {
+      name: "ordermanage",
+      path: "/ordermanage",
+      component: OrderManage
+    }
   ]
 })
