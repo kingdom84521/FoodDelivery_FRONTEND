6161f9a6741e745ee24ed9ba3eb482256158b165
BlueJoeR <joe8773@gmail.com>
2019-11-23 04:47:44 +0800

    Set menu router

--- client/src/router.js
+++ client/src/router.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Home from './views/Home.vue'
-import Menu from '@/views/Menu.vue'
 import Order from '@/views/Order.vue'
-import Submit from '@/views/Submit.vue'
+import Menu from '@/views/Menu.vue'
+// import Submit from '@/views/Submit.vue'
+
+import Test from '@/components/Test.vue'
 
 Vue.use(Router)
 
@@ -11,25 +12,30 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
-    {
-      path: '/menu',
-      name: 'menu',
-      component: Menu
-    },
     {
       path: '/order',
       name: 'order',
       component: Order
     },
     {
-      path: '/submit',
-      name: 'submit',
-      component: Submit
-    }
+      path: '/store/:storId',
+      name: 'store',
+      component: Menu
+    },
+    // {
+    //   path: '/menu',
+    //   name: 'menu',
+    //   component: Menu
+    // },
+    // {
+    //   path: '/submit',
+    //   name: 'submit',
+    //   component: Submit
+    // },
+    {
+      path: '/test',
+      name: 'test',
+      component: Test
+    },
   ]
 })
