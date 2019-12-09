3ca606bd7e517a7c2c0d2640304184b347f37f5c
bleach12326 <bleach12326@gmail.com>
2019-12-09 09:14:42 +0800

    OrderManage.vue fixed.

--- client/src/views/OrderManage.vue
+++ client/src/views/OrderManage.vue
@@ -1,8 +1,34 @@
 <template>
     <v-container fluid>
+        <v-navigation-drawer permanent>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="title">
+                        Application
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        subtext
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+            <v-list dense nav>
+                <v-list-item v-for="item in items" :key="item.title" link>
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+    </v-navigation-drawer>
+
         <v-tabs fixed-tabs>
             <v-tab v-for="i in tabname" :key="i">{{i}}</v-tab>
-
             <!-- Search -->
             <v-tab-item>
                 <v-text-field label="編號查詢" outlined clearable></v-text-field>
@@ -10,21 +36,15 @@
                 <row>
                     <v-text-field></v-text-field>
                 </row>
-
                 <v-btn ></v-btn>
             </v-tab-item>
-
             <!-- Modify -->
             <v-tab-item>
-                
             </v-tab-item>
-
             <!-- Refuse -->
             <v-tab-item>
-
             </v-tab-item>
         </v-tabs>
-
         <v-expansion-panels>
             <v-expansion-panel v-for="i in list" :key="i">
             <v-expansion-panel-header>{{i.no}}</v-expansion-panel-header>
@@ -40,6 +60,12 @@
 <script>
 export default {
     data: () => ({
+        items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+        
         tabname: ["訂單查詢", "訂單修改", "拒絕訂單"],
         list: [{
             no: "201911170001",
