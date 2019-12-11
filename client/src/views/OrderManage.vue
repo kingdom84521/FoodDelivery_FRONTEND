5caafa7fa4ab5b1f7f9411851b9c8246d44273b6
bleach12326 <bleach12326@gmail.com>
2019-12-11 12:34:35 +0800

    Store backend init.

--- client/src/views/OrderManage.vue
+++ client/src/views/OrderManage.vue
@@ -1,59 +1,65 @@
 <template>
     <v-container fluid>
-        <v-navigation-drawer permanent>
-            <v-list-item>
-                <v-list-item-content>
-                    <v-list-item-title class="title">
-                        Application
-                    </v-list-item-title>
-                    <v-list-item-subtitle>
-                        subtext
-                    </v-list-item-subtitle>
-                </v-list-item-content>
-            </v-list-item>
+        <v-row>
+            <v-col cols="2">
+                <v-navigation-drawer permanent>
+                    <v-list-item>
+                        <v-list-item-content>
+                            <v-list-item-title class="title">
+                                Application
+                            </v-list-item-title>
+                            <v-list-item-subtitle>
+                                subtext
+                            </v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
 
-            <v-divider></v-divider>
+                    <v-divider></v-divider>
 
-            <v-list dense nav>
-                <v-list-item v-for="item in items" :key="item.title" link>
-                    <v-list-item-icon>
-                        <v-icon>{{ item.icon }}</v-icon>
-                    </v-list-item-icon>
+                    <v-list dense nav>
+                        <v-list-item v-for="item in items" :key="item.title" link>
+                            <v-list-item-icon>
+                                <v-icon>{{ item.icon }}</v-icon>
+                            </v-list-item-icon>
 
-                    <v-list-item-content>
-                        <v-list-item-title>{{ item.title }}</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-    </v-navigation-drawer>
+                            <v-list-item-content>
+                                <v-list-item-title>{{ item.title }}</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list>
+                </v-navigation-drawer>
+            </v-col>
 
-        <v-tabs fixed-tabs>
-            <v-tab v-for="i in tabname" :key="i">{{i}}</v-tab>
-            <!-- Search -->
-            <v-tab-item>
-                <v-text-field label="編號查詢" outlined clearable></v-text-field>
-                <v-select label="菜單查詢" outlined multiple chips></v-select>
-                <row>
-                    <v-text-field></v-text-field>
-                </row>
-                <v-btn ></v-btn>
-            </v-tab-item>
-            <!-- Modify -->
-            <v-tab-item>
-            </v-tab-item>
-            <!-- Refuse -->
-            <v-tab-item>
-            </v-tab-item>
-        </v-tabs>
-        <v-expansion-panels>
-            <v-expansion-panel v-for="i in list" :key="i">
-            <v-expansion-panel-header>{{i.no}}</v-expansion-panel-header>
-            <v-expansion-panel-content>
-                {{i.menu}}
-                {{i.total}}
-            </v-expansion-panel-content>
-            </v-expansion-panel>
-        </v-expansion-panels>
+            <v-col cols="10">
+                <v-tabs fixed-tabs>
+                    <v-tab v-for="i in tabname" :key="i">{{i}}</v-tab>
+                    <!-- Search -->
+                    <v-tab-item>
+                        <v-text-field label="編號查詢" outlined clearable></v-text-field>
+                        <v-select label="菜單查詢" outlined multiple chips></v-select>
+                        <row>
+                            <v-text-field></v-text-field>
+                        </row>
+                        <v-btn ></v-btn>
+                    </v-tab-item>
+                    <!-- Modify -->
+                    <v-tab-item>
+                    </v-tab-item>
+                    <!-- Refuse -->
+                    <v-tab-item>
+                    </v-tab-item>
+                </v-tabs>
+                <v-expansion-panels>
+                    <v-expansion-panel v-for="i in list" :key="i">
+                        <v-expansion-panel-header>{{i.no}}</v-expansion-panel-header>
+                        <v-expansion-panel-content>
+                            {{i.menu}}
+                            {{i.total}}
+                        </v-expansion-panel-content>
+                    </v-expansion-panel>
+                </v-expansion-panels>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
